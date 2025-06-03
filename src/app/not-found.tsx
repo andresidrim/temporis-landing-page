cd4a9120d71a87a58b987fc8bb9d6a4c90fcbc3f
André Sidrim <andrecorreasidrim@gmail.com>
2025-06-03 20:45:56 -0300

    :sparkles: feat: add not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,13 @@
+import { Link } from "@/components/atoms";
+
 export default function NotFound() {
-  return <div>not found</div>;
+  return (
+    <div className="bg-temporis-white text-temporis-black font-lora flex flex-col items-center justify-center h-screen text-5xl md:text-6xl lg:text-7xl text-center">
+      <b className="text-6xl md:text-9xl mb-2">404</b>
+      Página não encontrada
+      <Link href="/" className="mt-20 text-xl md:text-3xl">
+        Voltar para a página principal
+      </Link>
+    </div>
+  );
 }
