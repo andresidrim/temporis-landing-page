cb73581ad76ffbc9d8a35dddeb2cd40e5b73fa2c
DanielMarcelino65 <danmarcelino6573@gmail.com>
2025-06-05 15:59:24 -0300

    :bug: fix Fixed issue where vercel caches response

--- src/lib/getVisits.ts
+++ src/lib/getVisits.ts
@@ -2,7 +2,10 @@ import { Visit } from '@/types/visits';
 
 export const getAmazonHackingVisits = async () => {
   const response = await fetch(
-    'https://temporis-api.vercel.app/museums/01e03fd6-a020-43bd-bf1e-8cd27d0baf09/visits'
+    'https://temporis-api.vercel.app/museums/01e03fd6-a020-43bd-bf1e-8cd27d0baf09/visits',
+    {
+      cache: 'no-store',
+    }
   );
   if (!response.ok) {
     throw new Error('Failed to fetch Amazon hacking visits');
